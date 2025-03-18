4a7d812eeee7785b81344884334f79d7c150fcbd
kent <kent.ganteng2@gmail.com>
2025-03-18 15:27:25 +0800

    fixed vuln , removed unnecessary packages, upgraded to vite from CRA

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    server: {
+        port: 3000, // Same port as CRA for familiarity
+        open: true,
+    },
+    esbuild: {
+        loader: "jsx",
+        include: /src\/.*\.jsx?$/,
+        exclude: [],
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx',
+            },
+        },
+    },
+}); 
